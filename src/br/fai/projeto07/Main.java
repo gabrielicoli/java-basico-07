834db5d48bfe6c4523ed85f8b2190510d39a5e02
gabrieli_coli <gabrielicolisi@hotmail.com>
2020-03-09 12:05:01 -0300

    quarto commit

--- src/br/fai/projeto07/Main.java
+++ src/br/fai/projeto07/Main.java
@@ -11,26 +11,27 @@ public class Main {
 
 	private void start() {
 		
+		operacoes(5, 5);
+		operacoes(10.27 , 10.42);
+		operacoes("44", "Meu namorado calça número");
 			}
 	private void operacoes(int num, int num1) {
-		num=5;
-		num1=5;
+		
+		
 		num= num+num1;
 		System.out.println("O resultado da operação e: "+num);
 	}
     private void operacoes(double num, double num1) {
     	
-		num=10.27;
-		num1=10.42;
+		
 		
 		num=num+num1;
 		 
 		System.out.println("O resultado da operação e: "+num);
 			}
 	private void operacoes(String num, String texto) {
-		num = "44";
-		texto = "Meu namorado calça número";
-		String result= num.concat(texto);
+		
+		String result= texto.concat(num);
 		
 		System.out.println("O resultado da operação e: "+result);
 	}
