5f17ae3a8c8ed9b246c45108f12b7cbb96f963cd
gabrieli_coli <gabrielicolisi@hotmail.com>
2020-03-09 11:37:23 -0300

    segundo commit

--- src/br/fai/projeto07/Main.java
+++ src/br/fai/projeto07/Main.java
@@ -3,8 +3,32 @@ package br.fai.projeto07;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Main app = new Main();
+		app.start();
+		
 	}
 
+	private void start() {
+		
+		
+	}
+	private void operacoes(int num, int num1) {
+		num=5;
+		num1=5;
+		num= num+num1;
+		System.out.println("O resultado da operação e: "+num);
+	}
+    private void operacoes(double num, double num1) {
+    	
+		num=10.27;
+		num1=10.42;
+			}
+	private void operacoes(String num, String texto) {
+		num = "44";
+		texto = "Exercicio de numero 7";
+		
+		
+	}
+		
+	
 }
